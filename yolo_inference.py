9da0b370e3ea35fc3b650e02cb81eb0cbf501b14
Richard Zhang <zhangweiyebellevue@gmail.com>
2024-03-24 17:16:13 -0700

    Preliminary Identifcation

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-result = model.predict('inputs/Demo_1.png', save=True)
+result = model.predict('inputs/Demo_2.mp4', save=True)
 
 # Visualize the predictions of the model on various objects within given inputs
 print(result)
