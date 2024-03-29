69cc0443962bee45dd992020a2346932ef5802f5
Richard Zhang <zhangweiyebellevue@gmail.com>
2024-03-29 12:33:12 -0700

    Object Tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -3,12 +3,12 @@ import torchvision
 
 from ultralytics import YOLO
 
-model = YOLO('models/best.pt')
+model = YOLO('yolov8x')
 
-result = model.predict('inputs/Demo_2.mp4', conf=0.2, save=True)
+result = model.track('inputs/Demo_2.mp4', conf=0.2, save=True)
 
 # Visualize the predictions of the model on various objects within given inputs
-print(result)
-print("Boxes: ")
-for box in result[0].boxes:
-  print(box)
+# print(result)
+# print("Boxes: ")
+# for box in result[0].boxes:
+#   print(box)
